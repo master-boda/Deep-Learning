ebc02f6ce60b6dcaacda54fbe3b41e5fb1aff23c
master-boda <vicente.miranda2013@gmail.com>
2024-11-28 20:12:40 +0000

    comments

--- src/main.py
+++ src/main.py
@@ -8,17 +8,24 @@ import tensorflow as tf
 
 # For releasing the GPU memory (only useful if using CUDA)
 from numba import cuda
+import numpy as np
 
 # CUDA (Nvidia GPU Computing)
 if len(tf.config.list_physical_devices('GPU')) > 0:
-    gpus = tf.config.experimental.list_physical_devices('GPU')
+    gpus = tf.config.list_physical_devices('GPU')
     print("Num GPUs Available: ", len(gpus))
     
     device = cuda.get_current_device()
     device.reset() # Dump the memory contents to free up the memory (it accumulates over the session)
     
-    for gpu in gpus:
-        tf.config.experimental.set_memory_growth(gpu, True) # Let Tensorflow allocate memory as needed
+    tf.config.experimental.set_memory_growth(gpus[0], True)
+
+    tf.config.set_logical_device_configuration(
+    gpus[0], 
+    [tf.config.LogicalDeviceConfiguration(memory_limit=8192)])  # Limit to 4GB
+
+    tf.compat.v1.disable_eager_execution()
+
 
 X_train, y_train, X_test, y_test, X_val, y_val = preproc_pipeline(desired_magnification='200X', 
                                                     image_resolution=(224, 224), 
@@ -26,7 +33,7 @@ X_train, y_train, X_test, y_test, X_val, y_val = preproc_pipeline(desired_magnif
 
 model = binary_classification_baseline_model(input_shape=(224, 224, 3))
 
-fitted_model = train_model(X_train, y_train, X_val, y_val, model, epochs=8)
+fitted_model = train_model(X_train, y_train, X_val, y_val, model, epochs=8, data_gen=True)
 
 test_loss, test_acc = fitted_model.evaluate(X_test, y_test)
 print('Test accuracy:', test_acc)
--- src/utils/modeling.py
+++ src/utils/modeling.py
@@ -1,25 +1,27 @@
 import numpy as np
 import tensorflow as tf
 
-from keras.models import Sequential
-from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
-from keras.preprocessing.image import ImageDataGenerator
 from sklearn.utils import shuffle
-from keras.callbacks import EarlyStopping, ModelCheckpoint
-
 from sklearn.utils import class_weight
-from sklearn.metrics import f1_score, accuracy_score
 
-def train_model(X_train, y_train, X_val, y_val, model, epochs=10, batch_size=32, early_stopping_patience=4):
-    # Class weights because our problem is unbalanced
-    class_weights = class_weight.compute_class_weight('balanced', 
-                                                      classes=np.unique(y_train), 
-                                                      y=y_train)
+from keras.callbacks import EarlyStopping
+
+def train_model(X_train, y_train, X_val, y_val, model, epochs=10, batch_size=32, early_stopping_patience=3, data_gen=True):
+    # class weights because our problem is unbalanced
+    class_weights = class_weight.compute_class_weight('balanced', classes=np.unique(y_train), y=y_train) # np.unique(y_train) possibilitates usage for both binary and multiclass classification
     
     class_weights = {i: weight for i, weight in enumerate(class_weights)}
     
+    # shuffle the data before training (it was not done in the preproc_pipeline function)
     X_train, y_train = shuffle(X_train, y_train)
+    X_val, y_val = shuffle(X_val, y_val)
 
-    model.fit(X_train, y_train, epochs=epochs, class_weight=class_weights, validation_data=(X_val, y_val), batch_size=batch_size, callbacks=[EarlyStopping(patience=early_stopping_patience)])
+    model.fit(
+        X_train, y_train,
+        epochs=epochs,
+        class_weight=class_weights,
+        validation_data=(X_val, y_val),
+        callbacks=[EarlyStopping(patience=early_stopping_patience)])
+        
 
     return model
