f6fb3f0989c9cf5e028dd5c6c8df759a52880437
master-boda <vicente.miranda2013@gmail.com>
2024-11-29 18:01:30 +0000

    small function changes, created model tuning notebook

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from models.models import *
 
 import tensorflow as tf
 
-# For releasing the GPU memory (only useful if using CUDA)
+# for releasing the GPU memory (only useful if using CUDA)
 from numba import cuda
 import numpy as np
 
@@ -16,24 +16,45 @@ if len(tf.config.list_physical_devices('GPU')) > 0:
     print("Num GPUs Available: ", len(gpus))
     
     device = cuda.get_current_device()
-    device.reset() # Dump the memory contents to free up the memory (it accumulates over the session)
+    device.reset() # dump the memory contents to free up the memory (it accumulates over the session)
     
     tf.config.experimental.set_memory_growth(gpus[0], True)
 
     tf.config.set_logical_device_configuration(
     gpus[0], 
-    [tf.config.LogicalDeviceConfiguration(memory_limit=8192)])  # Limit to 4GB
+    [tf.config.LogicalDeviceConfiguration(memory_limit=8192)])  # limit to 4GB
 
     tf.compat.v1.disable_eager_execution()
 
+def main(model, classification_type, image_resolution, verbose=True):
 
-train_gen, val_gen, X_test, y_test, class_weights = preproc_pipeline(desired_magnification='200X', 
-                                                    image_resolution=(224, 224), 
-                                                    classification_type='binary')
+    magnifications =  {'40X' : {'accuracy': 0},
+                      '100X' : {'accuracy': 0},
+                      '200X' : {'accuracy': 0},
+                      '400X' : {'accuracy': 0}}
+    
+    for magnification in magnifications:
+        train_gen, val_gen, X_test, y_test, class_weights = preproc_pipeline(desired_magnification=magnification, 
+                                                    image_resolution=image_resolution, 
+                                                    classification_type=classification_type)
+        
+        model_instance = model(input_shape=(image_resolution[0], image_resolution[1], 3))
+        
+        fitted_model = train_model(train_gen, val_gen, model_instance, class_weights=class_weights, epochs=8)
+        
+        test_loss, test_acc = fitted_model.evaluate(X_test, y_test)
+        
+        if verbose:
+            print(f'Model: {model}, Magnification: {magnification}')
+            print(f'Test loss: ', test_loss)
+            print(f'Test accuracy: ', test_acc)
+        
+        magnifications[magnification]['accuracy'] = test_acc
+        
+    return magnifications
+        
+print(main(multiclass_classification_baseline_model, 'multiclass', (224, 224)))
 
-model = binary_classification_baseline_model(input_shape=(224, 224, 3))
-
-fitted_model = train_model(train_gen, val_gen, model, class_weights=class_weights, epochs=8)
-
-test_loss, test_acc = fitted_model.evaluate(X_test, y_test)
-print('Test accuracy:', test_acc)
+# 1st Try Results (accuracy, 8 epochs):
+# binary_classification_baseline_model: {'40X': 0.73333335, '100X': 0.71794873, '200X': 0.84437084, '400X': 0.86080587}
+# multiclass_classification_baseline_model : {'40X': 0.10666667, '100X': 0.3621795, '200X': 0.2615894, '400X': 0.35897437}
