66da3d39281537774366464026bd42d40dab1d1b
master-boda <vicente.miranda2013@gmail.com>
2024-12-01 14:17:56 +0000

    docstrings

--- src/utils/modeling.py
+++ src/utils/modeling.py
@@ -5,7 +5,20 @@ from sklearn.metrics import classification_report
 
 from keras.callbacks import EarlyStopping
 
-def train_model(train_gen, val_gen, model, epochs=10, early_stopping_patience=3, class_weights=None):
+def train_model(train_gen, val_gen, model, epochs=10, early_stopping_patience=5, class_weights=None):
+    """
+    Trains a given model using the provided training and validation data generators.
+    
+    Parameters:
+        - train_gen (Generator): The training data generator.
+        - val_gen (Generator): The validation data generator.
+        - model (tf.keras.Model): The model to be trained.
+        - epochs (int, optional): The number of epochs to train the model, the default value is 10.
+        - early_stopping_patience (int, optional): The number of epochs with no improvement (monitors 'val_loss') after which training will be stopped, the default value is 5.
+        - class_weights (dict, optional): Dictionary mapping class indices (integers) to a weight (float) value, used for weighting the loss function during training. Defaults to None.
+    Returns:
+        - model (tf.keras.Model): The trained model.
+    """
     
     model.fit(
         train_gen,
@@ -18,6 +31,19 @@ def train_model(train_gen, val_gen, model, epochs=10, early_stopping_patience=3,
     return model
 
 def get_classification_report(model, X_test, y_test):
+    """
+    Generates and prints a classification report for a given model and test data.
+
+    Parameters:
+        - model (object): The trained model used for making predictions.
+        - X_test (array-like): The test data features.
+        - y_test (array-like): The true labels for the test data.
+
+    Returns:
+        None
+    """
     y_pred = model.predict(X_test)
     y_pred = np.argmax(y_pred, axis=1)
     print(classification_report(y_test, y_pred))
+    
+    return None
--- src/utils/preproc.py
+++ src/utils/preproc.py
@@ -6,6 +6,18 @@ from keras.preprocessing.image import ImageDataGenerator
 from sklearn.utils import class_weight
 
 def resize_and_append(image_path, label, X, y, img_size):
+    """
+    Resize an image and append it along with its label to the provided lists.
+    
+    Parameters:
+        - image_path (str): The file path to the image to be resized.
+        - label (any): The label associated with the image.
+        - X (list): The list to which the resized image array will be appended.
+        - y (list): The list to which the label will be appended.
+        - img_size (tuple): The target size for resizing the image (width, height).
+    Returns:
+        None
+    """
     with Image.open(image_path) as img:
         img_resized = img.resize(img_size)
         img_array = np.array(img_resized)
@@ -14,6 +26,20 @@ def resize_and_append(image_path, label, X, y, img_size):
         y.append(label)
         
 def normalize_pixels(X_train, X_test, X_val):
+    """
+    Normalize pixel values of training, testing, and validation datasets.
+    Scales the pixel values of the input datasets to the range [0, 1]
+    by dividing each pixel value by 255.0 (max pixel intensity).
+    
+    Parameters:
+        - X_train (numpy.ndarray): The training dataset with pixel values.
+        - X_test (numpy.ndarray): The testing dataset with pixel values.
+        - X_val (numpy.ndarray): The validation dataset with pixel values.
+    Returns:
+        - X_train (numpy.ndarray): Normalized training set images.
+        - X_test (numpy.ndarray): Normalized testing set images.
+        - X_val (numpy.ndarray): Normalized validation set images.
+    """
     X_train = X_train / 255.0
     X_test = X_test / 255.0
     X_val = X_val / 255.0
@@ -21,6 +47,18 @@ def normalize_pixels(X_train, X_test, X_val):
     return X_train, X_test, X_val
     
 def label_encode(y_train, y_test, y_val):
+    """
+    Encodes the labels of the training, testing, and validation datasets using label encoding.
+    
+    Parameters:
+        - y_train (array-like): The labels for the training dataset.
+        - y_test (array-like): The labels for the testing dataset.
+        - y_val (array-like): The labels for the validation dataset.
+    Returns:
+        - y_train (numpy.ndarray): Encoded training set labels.
+        - y_test (numpy.ndarray): Encoded testing set labels.
+        - y_val (numpy.ndarray): Encoded validation set labels.
+    """
     le = LabelEncoder()
     y_train = le.fit_transform(y_train)
     y_test = le.fit_transform(y_test)
@@ -29,6 +67,25 @@ def label_encode(y_train, y_test, y_val):
     return y_train, y_test, y_val
 
 def load_and_preprocess_data(csv_path, desired_magnification, image_resolution, label_column):
+    """
+    Load and preprocess image data from the CSV file containing the image metadata.
+    This function reads image data paths and labels from the CSV file, filters the data based on the desired magnification,
+    resizes the images to the specified resolution, sorts the data into training, testing, and validation arrays.
+    The images are then normalized, and the labels are encoded.
+    
+    Parameters:
+        - csv_path (str): Path to the image metadata CSV.
+        - desired_magnification (int): The magnification level to filter the images (40X, 100X, 200X, 400X).
+        - image_resolution (tuple): The desired resolution to resize the images (width, height).
+        - label_column (str): The name of the column in the CSV file that contains the labels ('Benign or Malignant' or 'Cancer Type').
+    Returns:
+        - X_train (numpy.ndarray): Training set images.
+        - y_train (numpy.ndarray): Training set labels.
+        - X_test (numpy.ndarray): Testing set images.
+        - y_test (numpy.ndarray): Testing set labels.
+        - X_val (numpy.ndarray): Validation set images.
+        - y_val (numpy.ndarray): Validation set labels.
+    """
     df = pd.read_csv(csv_path)
     
     # select only the rows for the selected magnification (40X, 100X, 200X, 400X)
@@ -66,6 +123,18 @@ def load_and_preprocess_data(csv_path, desired_magnification, image_resolution, 
     return X_train, y_train, X_test, y_test, X_val, y_val
 
 def data_augmentation(X_train, y_train, datagen, augmented_images_per_image):
+    """
+    Perform data augmentation on the training dataset.
+    
+    Parameters:
+        - X_train (numpy.ndarray): Array of training images.
+        - y_train (numpy.ndarray): Array of training labels.
+        - datagen (ImageDataGenerator): Keras ImageDataGenerator instance for generating augmented images.
+        - augmented_images_per_image (int): Number of augmented images to generate per original image.
+    Returns:
+        - X_train_augmented (numpy.ndarray): Array of augmented training images.
+        - y_train_augmented (numpy.ndarray): Array of augmented training labels.
+    """
     augmented_images = []
     augmented_labels = []
 
@@ -99,6 +168,29 @@ def preproc_pipeline(desired_magnification,
                      augmented_images_per_image=5,
                      csv_path = 'image_metadata/updated_image_data.csv',
                      batch_size=32):
+    """
+    Preprocess image data.
+
+    This function loads image data from the CSV file containing image metadata, filters it based on the desired magnification,
+    resizes the images, normalizes pixel values, encodes labels, and optionally performs data augmentation on the training set.
+    It returns data generators for training and validation, as well as the test dataset and class weights.
+
+    Parameters:
+        - desired_magnification (int): The magnification level to filter the images (40X, 100X, 200X, 400X).
+        - image_resolution (tuple): The desired resolution to resize the images (width, height).
+        - classification_type (str, optional): The type of classification ('binary' or 'multiclass'). Defaults to 'binary'.
+        - use_data_augmentation (bool, optional): Whether to perform data augmentation on the training dataset. Defaults to False.
+        - augmented_images_per_image (int, optional): Number of augmented images to generate per original image. Defaults to 5.
+        - csv_path (str, optional): Path to the image metadata CSV file. Defaults to 'image_metadata/updated_image_data.csv'.
+        - batch_size (int, optional): The batch size for the data generators. Defaults to 32.
+
+    Returns:
+        - train_gen (Iterator): Data generator for the training dataset.
+        - val_gen (Iterator): Data generator for the validation dataset.
+        - X_test (numpy.ndarray): Array of test set images.
+        - y_test (numpy.ndarray): Array of test set labels.
+        - class_weights (dict): Dictionary of class weights to handle class imbalance.
+    """
     
     if classification_type == 'binary':
         label_column = 'Benign or Malignant'
